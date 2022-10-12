0ed4372a9b8d3872741712f8651c5f17f1298b9b
Sergio Ceron <sceronf@gmail.com>
2022-10-12 11:57:40 -0500

    Increase limit

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ const app = express();
 const mediatorRouter = new MediatorRouter();
 
 app.use( cors() );
-app.use( express.json( { limit: 152428800 } ) );
-app.use( express.urlencoded( { extended: false } ) );
+app.use( express.json( { limit: '50mb' }) );
+app.use( express.urlencoded( { extended: false, limit: '50mb' } ) );
 
 app.use( function( req, res, next ) {
 	res.setHeader( 'Strict-Transport-Security', 'max-age=15724800; includeSubDomains' );
