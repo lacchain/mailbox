c5f748e475712b22711f4c4e58ee3a008fc12850
Sergio Ceron <sceronf@gmail.com>
2022-11-09 22:11:10 -0600

    Increase limit to 100mb

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ const app = express();
 const mediatorRouter = new MediatorRouter();
 
 app.use( cors() );
-app.use( express.json( { limit: '50mb' }) );
-app.use( express.urlencoded( { extended: true, limit: '50mb' } ) );
+app.use( express.json( { limit: '100mb' }) );
+app.use( express.urlencoded( { extended: true, limit: '100mb' } ) );
 
 app.use( function( req, res, next ) {
 	res.setHeader( 'Strict-Transport-Security', 'max-age=15724800; includeSubDomains' );
